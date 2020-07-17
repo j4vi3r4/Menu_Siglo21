8f802373a979f25055b3c62c8278c95b2b92e5be
j4vi3r4 <ja.basaez@alumnos.duoc.cl>
2020-07-17 17:03:13 -0400

    nuevo diseño - splashscreen

--- Menu_Siglo21.Android/MainActivity.cs
+++ Menu_Siglo21.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Menu_Siglo21.Droid
 {
-    [Activity(Label = "Menu_Siglo21", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Menu_Siglo21", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
